46341e68293866dd36a1dba2ae5b4572ef39827d
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-29 21:47:16 -0300

    Algoritmo de Bolha

---
+++ BubbleSort.h
@@ -0,0 +1,31 @@
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ***** FUNÇOES DE ORDENAÇÃO ***** */
+
+// Ordenação por bolha em C
+
+// O bubble sort, ou ordenação por flutuação (literalmente "por bolha"), é um algoritmo de ordenação dos mais simples.
+// A ideia é percorrer o vetor diversas vezes, a cada passagem fazendo flutuar para o topo o maior elemento da sequência. 
+// Essa movimentação lembra a forma como as bolhas em um tanque de água procuram seu próprio nível, e disso vem o nome do algoritmo.
+
+// No melhor caso, o algoritmo executa n {\displaystyle n} {\displaystyle n} operações relevantes, onde n representa o número de elementos 
+// do vetor. No pior caso, são feitas n 2 {\displaystyle n^{2}} {\displaystyle n^{2}} operações. A complexidade desse algoritmo é de 
+// Ordem quadrática. Por isso, ele não é recomendado para programas que precisem de velocidade e operem com quantidade elevada de dados. 
+
+void BubbleSort (int vet[], int tam){
+	int mem, exc, i, j;
+	exc = 1; 								/*Será a verificação de troca em casa passada*/
+	for(j=tam-1; (j>=1) && (exc==1); j--){
+		exc = 0; 							/*Se continuar valendo 0 na próxima vez, então não houve troca e encerra*/
+		for(i=0; i < j; i++){
+			if(vet[i] > vet[i+1]){
+				mem = vet[i];
+				vet[i] = vet[i+1];
+				vet[i+1] = mem;
+				exc = 1;					 /* Havendo troca, com exc=1 então vai continuar executando.*/
+			}
+		}
+	}
+}
+
+/* ***** FIM DAS FUNÇOES DE ORDENAÇÃO ***** */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
