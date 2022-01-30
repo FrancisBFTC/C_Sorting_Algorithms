d9b1a869a6e0def871c81a7a43e3a11a7561e38f
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-30 00:40:33 -0300

    Algoritmo de Donald Shell

---
+++ ShellSort.h
@@ -0,0 +1,35 @@
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ***** FUNÇOES DE ORDENAÇÃO ***** */
+
+// Ordenação por SHELLSORT
+
+// Criado por Donald Shell em 1959,[1] publicado pela Universidade de Cincinnati, Shell sort é o mais eficiente algoritmo 
+// de classificação dentre os de complexidade quadrática. É um refinamento do método de inserção direta.[2] O algoritmo difere 
+// do método de inserção direta pelo fato de no lugar de considerar o array a ser ordenado como um único segmento, ele considera 
+// vários segmentos sendo aplicado o método de inserção direta em cada um deles.
+// Basicamente o algoritmo passa várias vezes pela lista dividindo o grupo maior em menores. Nos grupos menores é aplicado o 
+// método da ordenação por inserção.
+
+void ShellSort (int *vet, int tam){
+	int i, j, value;
+	int gap = 1;
+	
+	while(gap < tam){
+		gap = 3 * gap + 1;
+	}
+	while(gap > 0){
+		for(i = gap; i < tam; i++){
+			value = vet[i];
+			j = i;
+			while(j > gap - 1 && value <= vet[j - gap]){
+				vet[j] = vet[j - gap];
+				j = j - gap;
+			}
+			vet[j] = value;
+		}
+		gap = gap / 3;
+	}
+}
+
+/* ***** FIM DAS FUNÇOES DE ORDENAÇÃO ***** */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */ 
