c5f8f5c1bb5cac892b9072d9f079ba31522da872
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-30 21:10:39 -0300

    Aplicação MAIN de chamada dos Algoritmos

--- Application.c
+++ Application.c
@@ -1,5 +1,6 @@
 #include "SortingAlgorithms.h"                  
 
+
 //Metodo Principal que executa as funções
 int main(void){
     system("title Algoritmos de Ordenacao");
@@ -13,15 +14,33 @@ int main(void){
                 }
                 noExist = false;
                 
-                int vetor[q];
-                putData(vetor, q);
+                int vetor[qt];
+				 char* data[ ] = {"dddd", "bbbb", "aaaa", "cccc"};
                 
-                printf("\n NUMEROS DESORDENADOS: \n");
-                showData(vetor, q);
+                if((strcmp(Method,"SMOOTHSORT") != 0)){
+                	putData(vetor, qt);
+                	
+                	printf("\n NUMEROS DESORDENADOS: \n");
+                	showData(vetor, qt);
+                	
+                	SortingAlgorithm(Method, vetor, data, qt);
+                	printf("\n \n NUMEROS ORDENADOS: \n");
+                	showData(vetor, qt);
+                	
+           		}else{
+           			
+           			printf("\n PALAVRAS DESORDENADAS: \n");
+                	showDataStr(data, qt);
+                	
+                	SortingAlgorithm("SMOOTHSORT", vetor, data, qt);
+                	printf("\n \n PALAVRAS ORDENADAS: \n");
+                	showDataStr(data, qt);
+				}
                 
-                SortingAlgorithm(Method, vetor, q);
-                printf("\n \n NUMEROS ORDENADOS: \n");
-                showData(vetor, q);
+                
+              //  int i;
+              //   for(i = 0; i < 5; i++)
+              //  	printf("{%s}", data[i]);
                 
                 printf("\nDeseja continuar? (s/n)\n");
                 v = getch();
