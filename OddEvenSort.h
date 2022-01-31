c6ea8d84925419876eed706f55ffc33a0c469150
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-30 22:44:57 -0300

    Algoritmo de Ímpar/Par

--- OddEvenSort.h
+++ OddEvenSort.h
@@ -3,8 +3,34 @@
 
 // Ordenação por ODDEVENSORT
 
-void OddEvenSort(int vector[], const int n){
-	// TODO Make the OddEvenSort Algorithm.
+/* 
+O Odd-even sort é um algoritmo de ordenação relativamente simples. É um algoritmo de ordenação por comparação baseado 
+no bubble sort com o qual compartilha muitas características. Ele funciona através da comparação de todos os pares indexados 
+(ímpar, par) de elementos adjacentes na lista e, se um par está na ordem errada (o primeiro é maior do que o segundo),
+ os elementos são trocados. 
+ */
+
+#define swap(A,B) aux = A; A = B; B = aux;
+
+void OddEvenSort(int vector[], int n){
+	int sorted = 0;
+	int x, aux;
+	while(!sorted){
+		sorted = 1;
+		for(x = 1; x < n-1; x += 2){
+			if(vector[x] > vector[x+1]){
+				swap(vector[x], vector[x+1]);
+				sorted = 0;
+			}
+		}
+		for(x = 0; x < n-1; x += 2){
+			if(vector[x] > vector[x+1]){
+				swap(vector[x], vector[x+1]);
+				sorted = 0;
+			}
+		}
+			
+	}
 }
 
 /* ***** FIM DAS FUNÇOES DE ORDENAÇÃO ***** */
