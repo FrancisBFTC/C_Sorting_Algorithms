6e3a927e4042962ef0a032e6b2be21d3c7a69bb2
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-31 01:49:36 -0300

    Algoritmo de Seleção
    É um algoritmo de ordenação por seleção, cuja responsabilidade é passar o menor valor do Array para a 1ª posição, após isto colocar o 2ª menor valor do Array na 2ª posição, 3ª menor valor na 3ª posição e assim é feito para os (n - 1) elementos restantes. Para o algoritmo saber qual é o menor valor em cada ordem (1ª, 2ª,... Nª), ele precisa de um laço externo que "seleciona" o valor do índice inicial e um laço interno que percorre todo o Array, quando é encontrado o 1ª menor valor, o algoritmo seleciona a posição atual onde o menor valor está e faz a troca de valores das posições selecionadas, a partir daí o próximo índice inicial é selecionado pelo laço externo e o mesmo processo novamente é feito com o laço interno, até que sobre os 2 últimos valores restantes, que se forem menor, a troca será feita, se não, continuarão como estão.

--- SelectionSort.h
+++ SelectionSort.h
@@ -1,24 +1,24 @@
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ***** FUNÇOES DE ORDENAÇÃO ***** */
 
-//Ordenação por seleção
+// Ordenação por Seleção
 //
 // A ordenação por seleção ou selection sort consiste em selecionar o menor item e colocar 
 // na primeira posição, selecionar o segundo menor item e colocar na segunda posição, segue 
 // estes passos até que reste um único elemento. Para todos os casos (melhor, médio e pior caso) 
 // possui complexidade C(n) = O(n²) e não é um algoritmo estável. 
 
-void SelectionSort (int vet[], int tam){
-    int i, j, min, x;
-    for (i = 0; i < tam-1; i++){
-        min = i;
-        for (j = i+1; j < tam; j++){
-            if (vet[j] < vet[min])
-               min = j;
-        }
-        x = vet[min];
-        vet[min] = vet[i];
-        vet[i] = x;
+#define SSWAP(A, B) aux = A; A = B; B = aux;
+
+void SelectionSort (int vector[], int size){
+    int i, j, minor, aux;							// Declara as variáveis
+    for (i = 0; i < size-1; i++){					// Laço externo que começa na 1ª posição
+        minor = i;									// seleciona o (1ª ou 2ª ou ... Nª) índice atual
+        for (j = i+1; j < size; j++){				// Laço interno que percorre todo o array começando pelo índice após o selecionado
+            if (vector[j] < vector[minor])			// Se o item atual for menor que o item na Nª posição selecionada (índice atual)
+               minor = j;							// Redefine a variável "menor" para o índice com o menor item
+        }											// Itera no laço interno até que o índice seja igual ao tamanho do array
+        SSWAP(vector[minor], vector[i]);		 	// Faz a troca do menor valor selecionado com a Nª posição selecionada
     }
 }
 
