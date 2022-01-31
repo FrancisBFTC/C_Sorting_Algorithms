b910942b74409b9fca7e365c74ad135f1a8a32c2
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-01-30 23:13:15 -0300

    Algoritmo Humorístico "Estou com Sorte"

--- BogoSort.h
+++ BogoSort.h
@@ -1,10 +1,45 @@
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ***** FUNÇOES DE ORDENAÇÃO ***** */
 
-// Ordenação por BOGOSORT
+// Ordenação por BOGOSORT (CaseSort ou Estou com Sorte)
 
-void BogoSort(int vector[], int n){
-	// TODO Make the Fun/Humoristic BogoSort Algorithm.
+/* Bogosort (também conhecido como CaseSort ou Estou com Sort), é um algoritmo de ordenação extremamente ineficiente. 
+É baseado na reordenação aleatória dos elementos. Não é utilizado na prática, mas pode ser usado no ensino de algoritmos 
+mais eficientes. Seu nome veio do engraçado termo quantum bogodynamics e, ultimamente, a palavra bogus. 
+
+Esse algoritmo é probabilístico por natureza. Se todos os elementos a serem ordenados são distintos, a complexidade 
+esperada é O(n × n!). O tempo exato de execução esperado depende do quantos diferentes valores de elementos ocorrem, 
+e quantas vezes cada um deles ocorre, mas para casos não-triviais o tempo esperado de execução é exponencial ou 
+super-exponencial a n.
+
+Ele termina pela mesma razão do teorema do macaco infinito; existe alguma probabilidade de que aconteça a permutação correta, 
+dado que em um infinito número de tentativas fatalmente a encontrará. Deve-se notar que com os algoritmos geradores de números 
+pseudo-aleatórios, que têm um número finito de estados e não são realmente aleatórios, o algoritmo pode nunca terminar para certos 
+conjuntos de valores a serem ordenados. Bogosort é um algoritmo de ordenação não estável. 
+*/
+
+bool is_sorted(int *a, int n)
+{
+  while ( --n >= 1 ) {
+    if ( a[n] < a[n-1] ) return false;
+  }
+  return true;
+}
+ 
+void shuffle(int *a, int n)
+{
+  int i, t, r;
+  for(i=0; i < n; i++) {
+    t = a[i];
+    r = rand() % n;
+    a[i] = a[r];
+    a[r] = t;
+  }
+}
+ 
+void BogoSort(int *vector, int tam)
+{
+  while ( !is_sorted(vector, tam) ) shuffle(vector, tam);
 }
 
 /* ***** FIM DAS FUNÇOES DE ORDENAÇÃO ***** */
