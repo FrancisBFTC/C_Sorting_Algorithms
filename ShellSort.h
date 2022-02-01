90132067547fd009747b4de429c36f0e160dc589
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-02-01 17:42:45 -0300

    Algoritmo de Donald Shell
    Criado por Donald Shell em 1959, publicado pela Universidade de Cincinnati, Shell Sort é o mais eficiente algoritmo de classificação dentre os de complexidade quadrática. É um refinamento do método de inserção direta. O algoritmo difere do método de inserção direta pelo fato de no lugar de considerar o Array a ser ordenado como um único segmento, ele considera vários segmentos sendo aplicado o método de inserção direta em cada um deles. Basicamente o algoritmo passa várias vezes pela lista dividindo o grupo maior em menores. Nos grupos menores é aplicado o método da ordenação por inserção.

--- ShellSort.h
+++ ShellSort.h
@@ -3,8 +3,8 @@
 
 // Ordenação por SHELLSORT
 
-// Criado por Donald Shell em 1959,[1] publicado pela Universidade de Cincinnati, Shell sort é o mais eficiente algoritmo 
-// de classificação dentre os de complexidade quadrática. É um refinamento do método de inserção direta.[2] O algoritmo difere 
+// Criado por Donald Shell em 1959, publicado pela Universidade de Cincinnati, Shell sort é o mais eficiente algoritmo 
+// de classificação dentre os de complexidade quadrática. É um refinamento do método de inserção direta. O algoritmo difere 
 // do método de inserção direta pelo fato de no lugar de considerar o array a ser ordenado como um único segmento, ele considera 
 // vários segmentos sendo aplicado o método de inserção direta em cada um deles.
 // Basicamente o algoritmo passa várias vezes pela lista dividindo o grupo maior em menores. Nos grupos menores é aplicado o 
