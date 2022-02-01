c8541702ec37a99578a663bd63f97ee7f59b8cf9
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-02-01 17:37:47 -0300

    Algoritmo com Head/Pilha
    O heapsort utiliza uma estrutura de dados chamada heap, para ordenar os elementos à medida que os insere na estrutura. Assim, ao final das inserções, os elementos podem ser sucessivamente removidos da raiz da heap, na ordem desejada, lembrando-se sempre de manter a propriedade de max-heap.  A heap pode ser representada como uma árvore (uma árvore binária com propriedades especiais) ou como um vetor. Para uma ordenação decrescente, deve ser construída uma heap mínima (o menor elemento fica na raiz). Para uma ordenação crescente, deve ser construído uma heap máxima (o maior elemento fica na raiz).

--- HeapSort.h
+++ HeapSort.h
@@ -9,14 +9,14 @@
 // seu desempenho em pior cenário é praticamente igual ao desempenho em cenário médio. Alguns algoritmos de ordenação rápidos têm 
 // desempenhos espectacularmente ruins no pior cenário, quer em tempo de execução, quer no uso da memória. 
 
-// O heapsort trabalha no lugar e o tempo de execução em pior cenário para ordenar n elementos é de O (n lg n). Lê-se logaritmo 
+// O heapsort que trabalha no lugar e o tempo de execução em pior cenário para ordenar n elementos é de O (n log n). Lê-se logaritmo 
 // (ou log) de "n" na base 2. Para valores de n, razoavelmente grandes, o termo log n é quase constante, de modo que o tempo de 
 // ordenação é quase linear com o número de itens a ordenar.
 // O heapsort utiliza uma estrutura de dados chamada heap, para ordenar os elementos à medida que os insere na estrutura. Assim,
 // ao final das inserções, os elementos podem ser sucessivamente removidos da raiz da heap, na ordem desejada, lembrando-se sempre 
 // de manter a propriedade de max-heap. 
 
-// A heap pode ser representada como uma árvore (uma árvore binária com propriedades especiais[1]) ou como um vetor. Para uma ordenação 
+// A heap pode ser representada como uma árvore (uma árvore binária com propriedades especiais) ou como um vetor. Para uma ordenação 
 // decrescente, deve ser construída uma heap mínima (o menor elemento fica na raiz). Para uma ordenação crescente, deve ser construído 
 // uma heap máxima (o maior elemento fica na raiz). 
 
