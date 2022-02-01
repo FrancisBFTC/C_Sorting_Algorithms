9b3da0ffdca6686c585c23d87b0cefe1a8198d9d
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-02-01 16:39:42 -0300

    Algoritmo Rápido de Divisão e Conquista
    O QuickSort é um algoritmo rápido e eficiente baseando na estratégia de "Divisão e Conquista" que consiste em dividir o vetor em 2 metades através de um elemento chamado "Pivô". Este elemento fica na metade do vetor principal (Ou sub-lista), o QuickSort se encarrega de percorrer todo o vetor colocando os elementos menores que o pivô na metade esquerda e os elementos maiores que o pivô na metade direita, após isto, é feito uma ou mais chamadas recursivas da função para fazer o mesmo procedimento na metade esquerda, dividindo e ordenando várias vezes até que toda a metade esquerda do vetor principal esteja ordenada. Uma 2ª chamada recursiva é feita para ordenar todos as metades direitas com o mesmo processo, até que o algoritmo comece a passar pro lado direito do vetor principal.

--- QuickSort.h
+++ QuickSort.h
@@ -1,7 +1,9 @@
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 /* ***** FUNÇOES DE ORDENAÇÃO ***** */
 
-// O algoritmo quicksort é um método de ordenação muito rápido e eficiente, inventado por C.A.R. Hoare em 1960[1], 
+// Ordenação por QUICKSORT
+
+// O algoritmo quicksort é um método de ordenação muito rápido e eficiente, inventado por C.A.R. Hoare em 1960, 
 // quando visitou a Universidade de Moscovo como estudante. Naquela época, Hoare trabalhou em um projeto de tradução 
 // de máquina para o National Physical Laboratory. Ele criou o quicksort ao tentar traduzir um dicionário de inglês para russo, 
 // ordenando as palavras, tendo como objetivo reduzir o problema original em subproblemas que possam ser resolvidos mais fácil e rápido.
@@ -11,29 +13,29 @@
 // de chaves menores e maiores recursivamente até que a lista completa se encontre ordenada.
 
 void QuickSort (int vet[], int begin, int end){
-	int i, j, pivo, aux;
-	i = begin;
-	j = end - 1;
-	pivo = vet[(begin + end) / 2];
-	while(i <= j){
-		while(vet[i] < pivo && i < end){
-			i++;
+	int i, j, pivo, aux;                            // Declara as 4 variáveis necessárias
+	i = begin;                                      // Armazena em i o índice inicial
+	j = end - 1;                                    // Armazena em j o índice final (tamanho do vetor - 1)
+	pivo = vet[(begin + end) / 2];                  // Pega o elemento pivo na metade do Vetor
+	while(i <= j){                                  // Enquanto índice inicial for menor ou igual ao índice final...
+		while(vet[i] < pivo && i < end){            // Enquanto o elemento atrás do pivo for menor que pivo e índice inicial menor que final...
+			i++;                                    // Incremente o índice inicial
+		}                                           // Volte ao while até que encontre um elemento maior que o pivo e mantém o índice inicial
+		while(vet[j] > pivo && j > begin){          // Enquanto o elemento afrente do pivo for maior que pivo e índice final maior que inicial...
+			j--;                                    // Decremente o índice final
+		}                                           // Volte ao while até que encontre um elemento menor que o pivo e mantém o índice final
+		if(i <= j){                                 // Se índice inicial for menor ou igual ao índice final (mantidos), realiza a troca dos elementos.
+			aux = vet[i];                           // Elemento de trás do pivo vai para aux
+			vet[i] = vet[j];                        // Posição do elemento de trás do pivo fica com o elemento da frente do pivo
+			vet[j] = aux;                           // Posição do elemento da frente do pivo fica com o elemento de trás do pivo (aux)
+			i++;                                    // Incremente índice inicial (de trás do pivo)
+			j--;                                    // Decremente índice final (da frente do pivo)
 		}
-		while(vet[j] > pivo && j > begin){
-			j--;
-		}
-		if(i <= j){
-			aux = vet[i];
-			vet[i] = vet[j];
-			vet[j] = aux;
-			i++;
-			j--;
-		}
-	}
-	if(j > begin)
-		QuickSort(vet, begin, j+1);
-	if(i < end)
-		QuickSort(vet, i, end);
+	}                                               // Volte ao loop while até i e j estiver apontando pro pivo (Metade do Vetor)
+	if(j > begin)                                   // Se índice final j for maior que inicio do vetor (Sendo índice final do lado esquerdo)
+		QuickSort(vet, begin, j+1);                 // Chame recursivamente a mesma função fazendo o mesmo processo no lado esquerdo
+	if(i < end)                                     // Se índice inicial i for menor que final do vetor (Sendo índice inicial do lado direito)
+		QuickSort(vet, i, end);                     // Chame recursivamente a mesma função fazendo o mesmo processo no lado direito
 }
 
 /* ***** FIM DAS FUNÇOES DE ORDENAÇÃO ***** */
